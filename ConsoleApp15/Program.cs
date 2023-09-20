17ce86f0ed51512c2f97d684257e2976421a4799
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-20 23:52:00 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет создатель, как тебя зовут?");
+            string answeruser = Console.ReadLine();
+            Console.WriteLine("а как ты назовешь меня");
+            string namebot = Console.ReadLine();
+            Console.WriteLine($"Приятно познакомится {answeruser}. Меня зовут {namebot}- это ты меня так назвал");
+        }
+    }
+}
